d5b473e974e8c3a93652665e2adfeac2b30f0993
Leandro Andrade <eusouleoandrade@gmail.com>
2021-12-25 22:28:14 -0300

    Add UseCase test.

---
+++ Tests/Application.UnitTest/UseCases/GetCustomerTransactionsUseCaseHandlerTest.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace Application.UnitTest.UseCases
+{
+    public class GetCustomerTransactionsUseCaseHandlerTest
+    {
+        [Fact]
+        public void CheckSuccessHandler()
+        {
+            // Arranje
+
+            // Act
+
+            // Assert
+        }
+    }
+}
